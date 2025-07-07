17a413763e5a293168bc5b54e303ec357236f7ba
pritishkdash <118682774+pritishkdash@users.noreply.github.com>
2025-07-08 00:55:08 +0530

    Update main.py

--- diabetes_api/main.py
+++ diabetes_api/main.py
@@ -7,7 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],  # or ["*"] during dev
+    allow_origins=["*"],  # or ["*"] during dev
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
