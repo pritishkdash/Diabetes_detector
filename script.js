3edca3fd9555c0560f2c88d30853c257b4eeb1b3
pritishkdash <118682774+pritishkdash@users.noreply.github.com>
2025-07-08 00:56:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("diabetes-form").addEventListener("submit", async functi
   const formData = new FormData(e.target);
   const jsonData = Object.fromEntries(formData.entries());
 
-  const response = await fetch("http://127.0.0.1:8000/predict", {
+  const response = await fetch("https://diabetes-detector.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
