360ec31c35e2f8307ee70d1dba4bcdce57ec98dd
Pritish Kumar Dash <dashpritish97@gmail.com>
2025-07-07 23:55:12 +0530

    Initial commit of my_folder

---
+++ diabetes_api/main.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],  # or ["*"] during dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+model = joblib.load("model.pkl")
+
+class DiabetesInput(BaseModel):
+    age: int
+    gender: str
+    polyuria: str
+    polydipsia: str
+    sudden_weight_loss: str
+    weakness: str
+    polyphagia: str
+    genital_thrush: str
+    visual_blurring: str
+    itching: str
+    irritability: str
+    delayed_healing: str
+    partial_paresis: str
+    muscle_stiffness: str
+    alopecia: str
+    obesity: str
+
+@app.post("/predict")
+def predict_diabetes(data: DiabetesInput):
+    df = pd.DataFrame([data.dict()])
+    df.rename(columns={
+    "age": "Age",
+    "gender": "Gender",
+    "polyuria": "Polyuria",
+    "polydipsia": "Polydipsia",
+    "sudden_weight_loss": "sudden weight loss",
+    "weakness": "weakness",
+    "polyphagia": "Polyphagia",
+    "genital_thrush": "Genital thrush",
+    "visual_blurring": "visual blurring",
+    "itching": "Itching",
+    "irritability": "Irritability",
+    "delayed_healing": "delayed healing",
+    "partial_paresis": "partial paresis",
+    "muscle_stiffness": "muscle stiffness",
+    "alopecia": "Alopecia",
+    "obesity": "Obesity"}, inplace=True)
+    df.replace(to_replace=["Male", "Female"], value=[1, 0], inplace=True)
+    df.replace(to_replace=["Yes", "No"], value=[1, 0], inplace=True)
+    prediction = model.predict(df)[0]
+    return {
+        "prediction": int(prediction),
+        "message": "Diabetic" if prediction == 1 else "Non-Diabetic"
+    }
---
+++ frontend/script.js
@@ -0,0 +1,18 @@
+document.getElementById("diabetes-form").addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const jsonData = Object.fromEntries(formData.entries());
+
+  const response = await fetch("http://127.0.0.1:8000/predict", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(jsonData)
+  });
+
+  const result = await response.json();
+  localStorage.setItem("prediction_result", JSON.stringify(result));
+  window.location.href = "result.html";
+});
